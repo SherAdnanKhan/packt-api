64831b4a453cbee8ecaec2f11a72e35d895e5256
Sam Thornton <samt@packt.com>
2020-11-09 12:59:36 +0000

    Corrects logged in navigation if visiting the homepage

--- src/resources/views/layouts/guest.blade.php
+++ src/resources/views/layouts/guest.blade.php
@@ -31,10 +31,18 @@
                         </a>
 
                         <div class="flex w-1/2 justify-end content-center">
-                            <a href="{{ route('login') }}" class="text-gray-600 ">Login</a>
-                            <a href="{{ route('register') }}" class="text-gray-500 ml-5">Register</a>
-                            <a href="{{ route('documentation') }}" class="text-gray-500 ml-5">Docs</a>
-                            <a href=""  class="text-gray-500 ml-5">FAQ's</a>
+                            @auth
+                                <a href="{{ route('dashboard') }}" class="text-gray-500 ml-5">Dashboard</a>
+                                <a href="{{ route('documentation') }}" class="text-gray-500 ml-5">Docs</a>
+                                <a href=""  class="text-gray-500 ml-5">FAQ's</a>
+                            @else
+                                <a href="{{ route('login') }}" class="text-gray-600 ">Login</a>
+                                <a href="{{ route('register') }}" class="text-gray-500 ml-5">Register</a>
+                                <a href="{{ route('documentation') }}" class="text-gray-500 ml-5">Docs</a>
+                                <a href=""  class="text-gray-500 ml-5">FAQ's</a>
+                            @endauth
+
+
                         </div>
 
                     </div>
