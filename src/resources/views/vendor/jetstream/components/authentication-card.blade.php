954aedae4faa449e2420ada72a27eb0532b53c50
Sam Thornton <samt@packt.com>
2020-11-11 14:27:30 +0000

    Removes middle alignment on register

--- src/resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ src/resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="h-screen flex flex-col justify-center items-center ">
+<div class="h-screen flex flex-col mt-4 items-center ">
     <div class="w-full sm:max-w-md  px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
