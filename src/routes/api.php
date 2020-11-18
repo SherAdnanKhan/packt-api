1b4628c7b4cc425b00bf677e154eb3cbb5bbb886
Sam Thornton <samt@packt.com>
2020-11-18 11:44:04 +0000

    fix: removed test throttle

--- src/routes/api.php
+++ src/routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['addAccessToken', 'auth:sanctum', 'throttle:2,1'])->prefix('v1')->group(function(){
+Route::middleware(['addAccessToken', 'auth:sanctum'])->prefix('v1')->group(function(){
 
    Route::get('/user', function(Request $request) {
        return $request->user();
